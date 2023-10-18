1c7e6ac6f0998c4e3b89addbc2c7540cd0ce22de
Tushar Dahiya <tusharxoxoxo@gmail.com>
2023-10-18 18:45:52 +0530

    adding the new disjoint set approach that i recently learned

--- disjoint_set_graph/disjoint_set_1.cpp
+++ disjoint_set_graph/disjoint_set_1.cpp
@@ -5,86 +5,109 @@ using namespace std;
 
 class DisjointSet
 {
-  vector < int >rank, parent, size;
+    vector < int >rank, parent, size;
 
-public:
+    public:
     DisjointSet (int n)
-  {
-    rank.resize (n + 1, 0);
-    parent.resize (n + 1, 0);
-    size.resize (n + 1, 0);
+    {
+        rank.resize (n + 1, 0);
+        parent.resize (n + 1, 0);
+        size.resize (n + 1, 0);
 
-    for (int i = 0; i < n; ++i)
-      {
-	parent[i] = i;
-	size[i] = 1;
-      }
-  }
+        for (int i = 0; i < n; ++i)
+        {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
 
-  int findUParent (int node)
-  {
-    if (node == parent[node])
-      return node;
-    return parent[node] = findUParent (parent[node]);
-  }
+    int findUParent (int node)
+    {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUParent (parent[node]);
+    }
 
-  void unionByRank (int u, int v)
-  {
-    int ultimateParentU = findUParent (u);
-    int ultimateParentV = findUParent (v);
+    void unionByRank (int u, int v)
+    {
+        int ultimateParentU = findUParent (u);
+        int ultimateParentV = findUParent (v);
 
-    if (ultimateParentU == ultimateParentV)
-      {
-	return;
-      }
+        if (ultimateParentU == ultimateParentV)
+        {
+            return;
+        }
 
-    if (rank[ultimateParentU] > rank[ultimateParentV])
-      {
-	parent[ultimateParentV] = ultimateParentU;
-      }
-    else if (rank[ultimateParentU] < rank[ultimateParentV])
-      {
-	parent[ultimateParentU] = ultimateParentV;
-      }
-    else
-      {
-	parent[ultimateParentV] = ultimateParentU;
-	rank[ultimateParentU]++;
-      }
-  }
+        if (rank[ultimateParentU] > rank[ultimateParentV])
+        {
+            parent[ultimateParentV] = ultimateParentU;
+        }
+        else if (rank[ultimateParentU] < rank[ultimateParentV])
+        {
+            parent[ultimateParentU] = ultimateParentV;
+        }
+        else
+        {
+            parent[ultimateParentV] = ultimateParentU;
+            rank[ultimateParentU]++;
+        }
+    }
+        void unionBySize (int u, int v)
+    {
+        int ultimateParentU = findUParent (u);
+        int ultimateParentV = findUParent (v);
+
+        if (ultimateParentU == ultimateParentV)
+        {
+            return;
+        }
+
+        if (rank[ultimateParentU] > rank[ultimateParentV])
+        {
+            parent[ultimateParentV] = ultimateParentU;
+            size[ultimateParentU] += ultimateParentV;
+        }
+        else         {
+            parent[ultimateParentU] = ultimateParentV;
+            size[ultimateParentV] += ultimateParentU;
+        }
+       
+    }
+
+
 
 };
 
-int
+    int
 main ()
 {
 
-  cout << "i am back to this" << endl;
+    cout << "i am back to this" << endl;
 
-  DisjointSet ds (7);
-  ds.unionByRank (1, 2);
-  ds.unionByRank (2, 3);
-  ds.unionByRank (4, 5);
-  ds.unionByRank (6, 7);
-  ds.unionByRank (5, 6);
+    DisjointSet ds (7);
+    ds.unionBySize(1, 2);
+    ds.unionBySize(2, 3);
+    ds.unionBySize(4, 5);
+    ds.unionBySize(6, 7);
+    ds.unionBySize(5, 6);
 
-  //checking wheather 3, 7 belong to the same set or not
-  if (ds.findUParent (3) == ds.findUParent (7))
+    //checking wheather 3, 7 belong to the same set or not
+    if (ds.findUParent (3) == ds.findUParent (7))
     {
-      cout << "Same\n";
-      //   cout << "fuck man";
+        cout << "Same\n";
+        //   cout << "fuck man";
     }
-  else
-    cout << "Not Same\n";
-  ds.unionByRank (3, 7);
-  if (ds.findUParent (3) == ds.findUParent (7))
+    else
+        cout << "Not Same\n";
+    ds.unionBySize(3, 7);
+    if (ds.findUParent (3) == ds.findUParent (7))
     {
-      cout << "Same\n";
-      //   cout << "fuck man";
+        cout << "Same\n";
+        //   cout << "fuck man";
     }
-  else
-    cout << "Not Same\n";
-  // cout << "why is this not gettign printed" << endl;
-  return 0;
+    else
+        cout << "Not Same\n";
+    // cout << "why is this not gettign printed" << endl;
+    return 0;
 }
 
